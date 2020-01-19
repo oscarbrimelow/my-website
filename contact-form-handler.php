9ea7ea3e23fa48b0e87cf774e3e94b1dcdd0b921
oscarbrimelow <60044440+oscarbrimelow@users.noreply.github.com>
2020-01-19 02:34:48 +0200

    Add files via upload

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -9,8 +9,8 @@ $email_from = 'oscarbrimelow@gmail.com';
 $email_subject = "New Form Submission";
 
 $email_body = "User Name: $name.\n".
-"User Email: $visitor_email.\n".
-"User Message: $message.\n";
+                "User Email: $visitor_email.\n".
+                   "User Message: $message.\n";
 
 
 $to = "oscarbrimelow@gmail.com";
@@ -21,7 +21,7 @@ $headers .= "Reply To: $visitor_email \r\n";
 
 mail($to,email_subject,$email_body,$headers);
 
-header("Location: Oscar's portfolio");
+header("Location: index.html");
 
 
 
